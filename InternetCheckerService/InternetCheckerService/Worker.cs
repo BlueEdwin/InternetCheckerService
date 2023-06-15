c097337f69c67428edf57a2d037bcc1895b1b9fe
EdwinWang1102 <wangedwin1102@gmail.com>
2023-06-15 20:21:54 +0800

    加入程式碼每次Service執行時記錄當下時間

--- InternetCheckerService/InternetCheckerService/Worker.cs
+++ InternetCheckerService/InternetCheckerService/Worker.cs
@@ -3,10 +3,36 @@ namespace InternetCheckerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly string logPath;
+        private StreamWriter checkerLogger = null!;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration config)
         {
             _logger = logger;
+            logPath = Path.Combine(
+                config.GetValue<string>("LogPath") ??
+                System.AppContext.BaseDirectory!,
+                "log.txt");
+        }
+
+        public void Log(string message)
+        {
+            if (checkerLogger == null) return;
+
+            checkerLogger.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {message}");
+            checkerLogger.Flush();
+        }
+
+        public override async Task StartAsync(CancellationToken stoppingToken)
+        {
+            checkerLogger = new StreamWriter(logPath, true);
+
+            _logger.LogInformation("Service started");
+            Log("Service started");
+
+            // 基底類別 BackgroundService 在 StartAsync() 呼叫 ExecuteAsync、
+            // 在 StopAsync() 時呼叫 stoppingToken.Cancel() 優雅結束
+            await base.StartAsync(stoppingToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -14,8 +40,20 @@ namespace InternetCheckerService
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                Log($"Worker running at: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+
+                await Task.Delay(10000, stoppingToken);
             }
+        }
+
+        // 服務停止時
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Service stopped");
+            Log("Service stopped");
+            checkerLogger.Dispose();
+            checkerLogger = null!;
+            await base.StopAsync(stoppingToken);
         }
     }
 }
