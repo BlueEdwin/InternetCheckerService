5da291e9a7bf28b66e62e1665760342710b555e2
EdwinWang1102 <wangedwin1102@gmail.com>
2023-06-15 20:21:08 +0800

    建立service name

--- InternetCheckerService/InternetCheckerService/Program.cs
+++ InternetCheckerService/InternetCheckerService/Program.cs
@@ -1,6 +1,11 @@
 using InternetCheckerService;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    //Sets service name
+    .UseWindowsService(options =>
+    {
+        options.ServiceName = ".NET Internet Checker Service";
+    })
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
