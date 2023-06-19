8b4704cb73a94f50787a91d900496203b365c1fe
EdwinWang1102 <wangedwin1102@gmail.com>
2023-06-19 19:08:09 +0800

    建立以System..timer.timer的服務

---
+++ InternetCheckerService/InternetCheckerService/TimedInternetChecker2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetCheckerService
+{
+    public class TimedInternetChecker2
+    {
+        #region Fields
+
+        private StreamWriter checkerLogger = null!;
+        private readonly ILogger<TimedInternetChecker> _logger;
+        private readonly string logPath;
+
+        #endregion
+
+        #region Constructor
+
+        public TimedInternetChecker2(ILogger<TimedInternetChecker> logger, IConfiguration config)
+        {
+            _logger = logger;
+            logPath = Path.Combine(
+                config.GetValue<string>("LogPath") ??
+                System.AppContext.BaseDirectory!,
+                "log.txt");
+        }
+
+        #endregion
+
+
+        #region
+
+        /// <summary>
+        /// Logging and settings
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            if (checkerLogger == null) return;
+
+            checkerLogger.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {message}");
+            checkerLogger.Flush();
+        }
+
+        #endregion
+    }
+}
