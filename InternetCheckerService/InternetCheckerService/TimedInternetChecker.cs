e8a5208ac70374a5868f6ea0ac2efd7f8af4793c
EdwinWang1102 <wangedwin1102@gmail.com>
2023-06-19 19:03:12 +0800

    加入註解

--- InternetCheckerService/InternetCheckerService/TimedInternetChecker.cs
+++ InternetCheckerService/InternetCheckerService/TimedInternetChecker.cs
@@ -7,13 +7,22 @@ using System.Threading.Tasks;
 
 namespace InternetCheckerService
 {
+    /// <summary>
+    /// Timed Internet Checker(System.Threadinig.Timer)
+    /// </summary>
     public class TimedInternetChecker : IHostedService, IDisposable
     {
+        #region FIelds
+
         private int executionCount = 0;
         private readonly ILogger<TimedInternetChecker> _logger;
         private Timer? _timer = null;
         private readonly string logPath;
         private StreamWriter checkerLogger = null!;
+
+        #endregion
+
+        #region Constructor
 
         public TimedInternetChecker(ILogger<TimedInternetChecker> logger, IConfiguration config)
         {
@@ -24,17 +33,27 @@ namespace InternetCheckerService
                 "log.txt");
         }
 
+        #endregion
+
+        #region Methods
+
         public Task StartAsync(CancellationToken stoppigToken)
         {
-            checkerLogger = new StreamWriter(logPath, true);
+            //Start logging
+            checkerLogger = new StreamWriter(logPath, true);  //Initialize logger
             Log("Timed Internet Checker Service start running");
             _logger.LogInformation("Timed Internet Checker Service running");
 
+            //Initialize Timer
             _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
 
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Work to do
+        /// </summary>
+        /// <param name="state"></param>
         private void DoWork(object? state)
         {
             var count = Interlocked.Increment(ref executionCount);
@@ -69,6 +88,10 @@ namespace InternetCheckerService
             _timer?.Dispose();
         }
 
+        /// <summary>
+        /// Check internet connection
+        /// </summary>
+        /// <returns></returns>
         public bool IsConnectedToInternet()
         {
             string host = "www.google.com.tw";
@@ -89,6 +112,10 @@ namespace InternetCheckerService
             }                
         }
 
+        /// <summary>
+        /// Logging and settings
+        /// </summary>
+        /// <param name="message"></param>
         public void Log(string message)
         {
             if (checkerLogger == null) return;
@@ -96,5 +123,7 @@ namespace InternetCheckerService
             checkerLogger.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {message}");
             checkerLogger.Flush();
         }
+
+        #endregion
     }
 }
