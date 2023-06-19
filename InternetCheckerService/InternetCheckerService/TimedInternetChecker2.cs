18fa01d1028cf15223c3ba4f3bc1d15ad5275e5c
EdwinWang1102 <wangedwin1102@gmail.com>
2023-06-19 19:46:48 +0800

    實作服務內容

--- InternetCheckerService/InternetCheckerService/TimedInternetChecker2.cs
+++ InternetCheckerService/InternetCheckerService/TimedInternetChecker2.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace InternetCheckerService
 {
-    public class TimedInternetChecker2
+    /// <summary>
+    /// Timed Internet Checker(System.Timers.Timer)
+    /// </summary>
+    public class TimedInternetChecker2 : IHostedService, IDisposable
     {
         #region Fields
 
+        private int executionCount = 0;
         private StreamWriter checkerLogger = null!;
         private readonly ILogger<TimedInternetChecker> _logger;
         private readonly string logPath;
+        private System.Timers.Timer? _timer = null;
 
         #endregion
 
@@ -24,13 +31,91 @@ namespace InternetCheckerService
             logPath = Path.Combine(
                 config.GetValue<string>("LogPath") ??
                 System.AppContext.BaseDirectory!,
-                "log.txt");
+                "log2.txt");
         }
 
         #endregion
 
+        #region Methods
 
-        #region
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            //Start Logging
+            checkerLogger = new StreamWriter(logPath, true);  //Initialize logger
+            Log("Timed Internet Checker Service start running");
+            _logger.LogInformation("Timed Internet Checker Service running");
+
+            //Initialize timer
+            _timer = new System.Timers.Timer(30000);
+            _timer.Elapsed += DoWork;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Internet Checker Service is stoping");
+
+            _timer?.Stop();
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        /// <summary>
+        /// Work to do
+        /// </summary>
+        /// <param name="state"></param>
+        private void DoWork(object? state, ElapsedEventArgs e)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+
+            _logger.LogInformation("Checker is working. Count: {Count}", count);
+
+            Log("Checker is working. Count: {Count}" + count.ToString());
+
+            bool pingResult = IsConnectedToInternet();
+
+            if (pingResult)
+            {
+                _logger.LogInformation($"Target host internet connected!" + DateTime.Now.ToString());
+            }
+            else
+            {
+                _logger.LogInformation("Internet connection failure!" + DateTime.Now.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Check internet connection
+        /// </summary>
+        /// <returns></returns>
+        public bool IsConnectedToInternet()
+        {
+            string host = "www.google.com.tw";
+
+            Ping p = new Ping();
+
+            PingReply reply = p.Send(host, 3000);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                Log("Target host is connected..." + DateTime.Now.ToString());
+                return true;
+            }
+            else
+            {
+                Log("Target host disconnected..." + DateTime.Now.ToString());
+                return false;
+            }
+        }
 
         /// <summary>
         /// Logging and settings
